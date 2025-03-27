e7d3a726aa46a7fb36ceab57cc82b959c1dcbf9e
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-03-27 18:25:00 +0800

    feat: Get the best option regardless of order
    Context: I'm breaking the problem down into sub problems and connect them later when all subproblems are solved

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,13 +2,48 @@
 
 def stock_picker(stock_prices)
   result = []
+  p stock_prices.sort
   # Get the lowest value on the array
   lowest_val = stock_prices.min
-  result << stock_prices.index(lowest_val)
+  lowest_val_idx = stock_prices.index(lowest_val)
+  result << lowest_val_idx
 
-  # Get the lowest value on the array
-  highest_val = stock_prices.max
-  result << stock_prices.index(highest_val)
+  # Get the highest value on the array
+
+  # Get the days after the lowest
+  days_after_lowest = stock_prices.slice(lowest_val_idx + 1, stock_prices.length - 1)
+
+  p days_after_lowest
+  p days_after_lowest.max
+  highest_val = days_after_lowest.max
+
+  highest_val_idx = days_after_lowest.index(highest_val) + 1 + lowest_val_idx
+  result << highest_val_idx
 end
 
-p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+def get_best_option(stock_prices)
+  best_option = 0
+  indexes = []
+
+  stock_prices.each do |prices|
+    stock_prices.each do |p|
+      stock_gain = prices - p
+      next unless stock_gain > best_option
+
+      best_option = stock_gain
+      indexes.clear
+      lowest_val = [prices, p].min
+      highest_val = [prices, p].max
+      indexes << stock_prices.index(lowest_val)
+      indexes << stock_prices.index(highest_val)
+    end
+  end
+
+  indexes
+end
+
+# p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+
+# p stock_picker([3, 30, 2, 27, 4, 14])
+
+p get_best_option([17, 3, 6, 9, 15, 8, 6, 1, 10])
