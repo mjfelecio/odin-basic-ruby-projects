f9c7bd99ad71f290809cb269045c4fa04d8b95c2
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-03-27 16:43:38 +0800

    fix: Method name typo

--- substrings.rb
+++ substrings.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: false
 
-def substring(string, dictionary)
+def substrings(string, dictionary)
   string.downcase! # Downcase the string so that it's case insensitive
   dictionary.each_with_object(Hash.new(0)) do |substr, hash|
     # this functionality is done by replacing the string with the uppercased substring
     # if something got replaced, that means that the string contains the substring in the dictionary
-    string.split do |word|
+    string.split.each do |word|
       hash[substr] += 1 if word.sub(/#{substr}/i, substr.upcase) != word
     end
   end
@@ -13,4 +13,4 @@ end
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-p substring("Howdy partner, sit down! How's it going?", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
