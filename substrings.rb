9ad143cd43ed434123292b354ac41faca4f1794f
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-03-27 16:21:13 +0800

    feat: Completed the substrings project
    This detects all of the substrings in the string provided by:
    - Spliting the string on it's white space and finding if each word
     contains the substring. If so, iterate the counter.
    - The program detects if the word contains a substring by applying a
    \#sub method on the string with the substring as the parameter.
    	- If the word contains the substring, then it's string will be replaced
    	by an uppercased substring.
    	- If the word is replaced, then it will no longer match the original word,
    	thus we increment the counter.
    
    To provide an example, here's the execution flow:
    	string = "BeLow", dictionary = "low"
    
    String gets downcased to ensure case-insensitiveness:
    	"BeLow" => "below"
    
    Since string contains "low" it becomes:
    	"beLOW"
    
    Thus:
    	"below" != "beLOW"
    
    So we increment the counter on that substring. Do these on each word
    contained in the string and we get the final hash!

---
+++ substrings.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: false
+
+def substring(string, dictionary)
+  string.downcase! # Downcase the string so that it's case insensitive
+  dictionary.each_with_object(Hash.new(0)) do |substr, hash|
+    # this functionality is done by replacing the string with the uppercased substring
+    # if something got replaced, that means that the string contains the substring in the dictionary
+    string.split do |word|
+      hash[substr] += 1 if word.sub(/#{substr}/i, substr.upcase) != word
+    end
+  end
+end
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+p substring("Howdy partner, sit down! How's it going?", dictionary)
